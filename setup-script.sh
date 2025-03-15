d1b158e9f346aecc81937da4fb95e6551f150322
kabir <kabir@rathena-50.local.vm>
2025-03-15 16:43:21 +0700

    set public ip properly

--- setup-script.sh
+++ setup-script.sh
@@ -31,62 +31,73 @@ if [[ ! -d  "$CONF_DIR/import" ]]; then
     mkdir -p "$CONF_DIR/import"
 fi
 
-function copy_tmpl() {
-    if [[ ! -f "$CONF_DIR/import/$1" && -f "$CONF_DIR/import-tmpl/$1" ]]; then
-        cp "$CONF_DIR/import-tmpl/$1" "$CONF_DIR/import/$1"
+
+function set_config() {
+    has_config=$(grep -E "^$2:.*$" $1)
+    if [[ ! -z $has_config ]]; then
+        sed -i "s/^$2:.*$/$2: $3/g" $1
+    else
+        printf "\n$2: $3\n" >> $1
     fi
 }
-
-for conf_file in "$CONF_DIR/import-tmpl/*"
-do
-    conf_file_name=$(basename $conf_file)
-    copy_tmpl $conf_file_name
-done
 
 # Configure MySQL connection
 INTER_CONFIG="$CONF_DIR/import/inter_conf.txt"
 
-function set_config() {
-    has_config=$(grep -E "^$1:.*$" $INTER_CONFIG)
-    if [[ ! -z $has_config ]]; then
-        sed -i "s/^$1:.*$/$1: $2/g" $INTER_CONFIG
-    else
-        printf "\n$1: $2" >> $INTER_CONFIG
-    fi
-}
+set_config $INTER_CONFIG login_server_ip $DB_HOST
+set_config $INTER_CONFIG login_server_port $DB_PORT
+set_config $INTER_CONFIG login_server_id $DB_USER
+set_config $INTER_CONFIG login_server_pw $DB_PASS
+set_config $INTER_CONFIG login_server_db $DB_NAME
 
-set_config login_server_ip $DB_HOST
-set_config login_server_port $DB_PORT
-set_config login_server_id $DB_USER
-set_config login_server_pw $DB_PASS
-set_config login_server_db $DB_NAME
+set_config $INTER_CONFIG ipban_db_ip $DB_HOST
+set_config $INTER_CONFIG ipban_db_port $DB_PORT
+set_config $INTER_CONFIG ipban_db_id $DB_USER
+set_config $INTER_CONFIG ipban_db_pw $DB_PASS
+set_config $INTER_CONFIG ipban_db_db $DB_NAME
 
-set_config ipban_db_ip $DB_HOST
-set_config ipban_db_port $DB_PORT
-set_config ipban_db_id $DB_USER
-set_config ipban_db_pw $DB_PASS
-set_config ipban_db_db $DB_NAME
+set_config $INTER_CONFIG char_server_ip $DB_HOST
+set_config $INTER_CONFIG char_server_port $DB_PORT
+set_config $INTER_CONFIG char_server_id $DB_USER
+set_config $INTER_CONFIG char_server_pw $DB_PASS
+set_config $INTER_CONFIG char_server_db $DB_NAME
 
-set_config char_server_ip $DB_HOST
-set_config char_server_port $DB_PORT
-set_config char_server_id $DB_USER
-set_config char_server_pw $DB_PASS
-set_config char_server_db $DB_NAME
+set_config $INTER_CONFIG map_server_ip $DB_HOST
+set_config $INTER_CONFIG map_server_port $DB_PORT
+set_config $INTER_CONFIG map_server_id $DB_USER
+set_config $INTER_CONFIG map_server_pw $DB_PASS
+set_config $INTER_CONFIG map_server_db $DB_NAME
 
-set_config map_server_ip $DB_HOST
-set_config map_server_port $DB_PORT
-set_config map_server_id $DB_USER
-set_config map_server_pw $DB_PASS
-set_config map_server_db $DB_NAME
+set_config $INTER_CONFIG web_server_ip $DB_HOST
+set_config $INTER_CONFIG web_server_port $DB_PORT
+set_config $INTER_CONFIG web_server_id $DB_USER
+set_config $INTER_CONFIG web_server_pw $DB_PASS
+set_config $INTER_CONFIG web_server_db $DB_NAME
 
-set_config web_server_ip $DB_HOST
-set_config web_server_port $DB_PORT
-set_config web_server_id $DB_USER
-set_config web_server_pw $DB_PASS
-set_config web_server_db $DB_NAME
+set_config $INTER_CONFIG log_db_ip $DB_HOST
+set_config $INTER_CONFIG log_db_port $DB_PORT
+set_config $INTER_CONFIG log_db_id $DB_USER
+set_config $INTER_CONFIG log_db_pw $DB_PASS
+set_config $INTER_CONFIG log_db_db $DB_NAME
 
-set_config log_db_ip $DB_HOST
-set_config log_db_port $DB_PORT
-set_config log_db_id $DB_USER
-set_config log_db_pw $DB_PASS
-set_config log_db_db $DB_NAME
+# Configure IP Addresses
+set_config "$CONF_DIR/import/login_conf.txt" bind_ip "0.0.0.0"
+set_config "$CONF_DIR/import/char_conf.txt" bind_ip "0.0.0.0"
+set_config "$CONF_DIR/import/map_conf.txt" bind_ip "0.0.0.0"
+set_config "$CONF_DIR/import/web_conf.txt" bind_ip "0.0.0.0"
+
+if [[ ! -z $LOGIN_HOST ]]; then
+    set_config "$CONF_DIR/import/char_conf.txt" login_ip "$LOGIN_HOST"
+fi
+
+if [[ ! -z $CHAR_HOST ]]; then
+    set_config "$CONF_DIR/import/map_conf.txt" char_ip "$CHAR_HOST"
+fi
+
+if [[ ! -z $CHAR_PUBLIC_HOST ]]; then
+    set_config "$CONF_DIR/import/char_conf.txt" char_ip "$CHAR_PUBLIC_HOST"
+fi
+
+if [[ ! -z $MAP_PUBLIC_HOST ]]; then
+    set_config "$CONF_DIR/import/map_conf.txt" map_ip "$MAP_PUBLIC_HOST"
+fi
